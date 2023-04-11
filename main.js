951b8ea12f5c369f2606757a4896e02ee272de15
thanh dang <dangthanh.fe@gmail.com>
2023-04-11 19:07:27 +0700

    home work js6

---
+++ main.js
@@ -0,0 +1,15 @@
+const images = document.querySelectorAll('img');
+
+images.forEach((img) => {
+    img.addEventListener('mouseover', () => {
+      img.style.transform = 'scale(1.1)';
+      img.style.transition = 'transform 0.3s ease-in-out';
+    });
+  
+    img.addEventListener('mouseout', () => {
+      img.style.transform = 'scale(1)';
+      img.style.transition = 'transform 0.3s ease-in-out';
+    });
+});
+  
+
